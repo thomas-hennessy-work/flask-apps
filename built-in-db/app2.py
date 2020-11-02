378e90d0721ad068c3470906fe973873849cb0fc
Thomas <thomas.louis.hennessy@gmail.com>
2020-11-02 14:21:57 +0000

    modified app2

--- built-in-db/app2.py
+++ built-in-db/app2.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password123@34.105.221.87/steelWorks"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 print(db)
