87628d5169cc251f98253bde3b7dfefa2b4a18c1
Thomas <thomas.louis.hennessy@gmail.com>
2020-11-02 14:24:15 +0000

    modifications

--- built-in-db/app2.py
+++ built-in-db/app2.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password123@34.105.221.87/steelWorks"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://storage.db"
 
 if __name__ == "__main__":
     app.run(debug=True)
