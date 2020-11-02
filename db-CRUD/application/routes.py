b3c41c7570db1070749be2db3e53885a9169872e
Thomas <thomas.louis.hennessy@gmail.com>
2020-11-02 16:00:53 +0000

    added CRUD tutorial

---
+++ db-CRUD/application/routes.py
@@ -0,0 +1,24 @@
+from application import app, db
+from application.models import Games
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to the database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>" + game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
